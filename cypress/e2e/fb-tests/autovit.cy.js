01c44a1ce89e720d0d5375bea7fd5985fc283941
alex fierascu <ioan.fierascu@gmail.com>
2025-08-22 13:34:45 +0200

    add homepage tests

--- cypress/e2e/fb-tests/autovit.cy.js
+++ cypress/e2e/fb-tests/autovit.cy.js
@@ -17,26 +17,9 @@ describe('autovit tests', () => {
         cy.get(homepage.homepageElements.mainPageElements.button_cauta_anunturi).contains(/Caută.*anunțuri/);
     });
 
-    xit('verificam ca putem completa formularul de login', () => {
-        // In addition to using the `get` command to get an element by selector,
-        // we can also use the `contains` command to get an element by its contents.
-        // However, this will yield the <label>, which is lowest-level element that contains the text.
-        // In order to check the item, we'll find the <input> element for this <label>
-        // by traversing up the dom to the parent element. From there, we can `find`
-        // the child checkbox <input> element and use the `check` command to check it.
-        cy.contains('Pay electric bill')
-            .parent()
-            .find('input[type=checkbox]')
-            .check()
+    it('verificam ca putem completa formularul de login', () => {
+        cy.log("ce smecher este testul asta")
 
-        // Now that we've checked the button, we can go ahead and make sure
-        // that the list element is now marked as completed.
-        // Again we'll use `contains` to find the <label> element and then use the `parents` command
-        // to traverse multiple levels up the dom until we find the corresponding <li> element.
-        // Once we get that element, we can assert that it has the completed class.
-        cy.contains('Pay electric bill')
-            .parents('li')
-            .should('have.class', 'completed')
     });
 
     xit('verificam elementele din header', () => {
